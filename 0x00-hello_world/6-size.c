8cf86e240d22b3a5704a664397a4340b75bc6a3f
manielpb <manuelpepr99@icloud.com>
2022-12-08 22:43:37 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of char: %lu byte(s)\n" ,sizeof(char));
+	printf("size of int: %lu byte(s)\n",sizeof(int));
+	printf("size of long int: %lu byte(s)\n",sizeof(long int));
+	printf("size of long long int: %lu byte(s)",sizeof(long long int));
+	printf("size of float: %lu byte(s)",sizeof(float));
+
+
+	return (0);
+
+}

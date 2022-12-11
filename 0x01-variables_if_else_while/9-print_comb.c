0eab92208ae91d268c15e60007a8c11820688766
manielpb <manuelpepr99@icloud.com>
2022-12-11 09:51:53 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,6 @@
 
 /**
  *   main - Prints all possible combinations of single-digit numbers.
- *   
  *   Return: Always 0.
  */
 int main(void)
@@ -13,9 +12,7 @@ int main(void)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-													continue;
-												
-				
+			continue;									
 	putchar(',');
 	putchar(' ');
 										

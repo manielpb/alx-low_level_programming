7c9b13b084afebed29042389c0e61a97322e6e4b
manielpb <manuelpepr99@icloud.com>
2022-12-20 12:25:02 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,12 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (count >= '\0')
-		break;
-	count++;
-}
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
 	for (count--; count >= 0; count--)
 		_putchar(s[count]);

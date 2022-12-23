afa084b97695a804fee4d2d00f9872d7af565630
manielpb <manuelpepr99@icloud.com>
2022-12-22 16:09:10 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,12 @@
  *  Return: the pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
-{
-	int index = 0, dest_len = 0;
+{	int i;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
 	return (dest);
 }
